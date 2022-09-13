d59893cab472856c8ce33439b88243e59d20201f
anaalvsr <analauraalves25@gmail.com>
2022-09-13 18:34:34 -0300

    feat(Banco): cria método para gerar novo número de conta

--- src/main/java/com/trybe/acc/java/caixaeletronico/Banco.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/Banco.java
@@ -1,5 +1,32 @@
 package com.trybe.acc.java.caixaeletronico;
 
-public class Banco {
+import java.util.ArrayList;
+import java.util.Random;
 
+public class Banco {
+  private ArrayList<String> contas = new ArrayList<>();
+
+  /** Método para gerar um novo número, único e de 10 dígitos, para contas. */
+  public String gerarNumeroNovaConta() {
+    Random random = new Random();
+    String numeroConta = "";
+
+    for (int i = 0; i < 10; i++) {
+      String digito = Integer.toString(random.nextInt());
+      numeroConta.concat(digito);
+    }
+
+    if (this.contas.contains(numeroConta)) {
+      return this.gerarNumeroNovaConta();
+    } else {
+      this.contas.add(numeroConta);
+
+      return numeroConta;
+    }
+  }
+
+  /** Método get do atributo provado contas, que armazena todas as contas. */
+  public ArrayList<String> getContas() {
+    return this.contas;
+  }
 }
