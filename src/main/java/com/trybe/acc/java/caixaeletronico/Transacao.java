9e0891de1d24c7cbf71e6cc759034acc8f38b162
anaalvsr <analauraalves25@gmail.com>
2022-09-14 17:51:46 -0300

    feat(Transacao): cria construtor da classe

--- src/main/java/com/trybe/acc/java/caixaeletronico/Transacao.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/Transacao.java
@@ -1,6 +1,20 @@
 package com.trybe.acc.java.caixaeletronico;
 
 public class Transacao {
+  private double quantia;
+  private String instante;
+  private String descricao;
+  private Conta conta;
 
+  /**
+   * Método construtor da classe.
+   * 
+   * @param quantia   // Recebe um valor double que representa a quantia alvo da
+   *                  transação.
+   * @param descricao // Recebe a descrição, em forma de String, da transação.
+   */
+  public Transacao(double quantia, String descricao) {
+    this.quantia = quantia;
+    this.descricao = descricao;
+  }
 }
-
